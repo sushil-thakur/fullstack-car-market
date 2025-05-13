4e56555dc247bcc7fbab389e26b499f3f05d3c8b
sushil thakur <sushilt2059@gmail.com>
2025-05-13 19:44:05 +0545

    push

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,6 +5,15 @@ function Category() {
     <div className='mt-40'>
         <h2 className='font-bold text-3xl text-center mb-6'>Browse By Type</h2>
 
+      <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-6 px-20'>
+        {Data.Category.map((category,index)=>(
+          <div>
+            <img src={category.icon}width={40} height={40}/> 
+            <h2>{category.name}</h2> 
+        
+  </div>
+        ))}
+      </div>
     </div>
   )
 }
